55e81f2710aa5e1aa417e17fa3fafd393615c007
Eru <eru193m@gmail.com>
2020-03-02 01:30:35 +0200

    process api

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+	'use strict';
+
 	//Wheel
-	const pointer = document.querySelector('.center_point');
 	const start = document.querySelector('#button_one');
 	const nextStep = document.querySelector('#button_two');
 	const modal_2 =	document.querySelector('#modal_2');
@@ -67,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function() {
 							opacity: 0.3,
 							duration: 100,
 							direction: 'normal',
-							easing: 'easeInOutExpo',
+							easing: 'easeOutExpo',
 							complete: function(){
 								anime({
 									targets: '#modal_2',
@@ -105,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				opacity: 0,
 				direction: 'normal',
 				autoplay: false,
-				easing: 'easeInOutQuad',
+				easing: 'easeInQuad',
 				complete: function(){
 					anime({
 						targets: '.overlay',
@@ -113,6 +114,15 @@ document.addEventListener("DOMContentLoaded", function() {
 						opacity: 1,
 						duration: 150,
 						easing: 'easeInOutExpo',
+						complete: function(){
+							anime({
+								targets: '.jump_card',
+								duration: 150,
+								delay: 50,
+								translateY: -50,
+								easing: 'easeInOutElastic'	
+							})
+						}
 					})
 				}
 			});
@@ -123,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				translateY: 64,
 				opacity: 0,
 				direction: 'normal',
-				easing: 'easeInOutQuad',
+				easing: 'easeInQuad',
 				autoplay: false,
 				begin: function(){
 					modal_2.style.display = 'none';
@@ -146,6 +156,7 @@ document.addEventListener("DOMContentLoaded", function() {
 								targets: targets_image,
 									duration: 150,
 									opacity: 0.3,
+									easing: 'easeOutExpo',
 									complete: function(){
 										anime({
 											targets: '#modal_3',
@@ -169,8 +180,8 @@ document.addEventListener("DOMContentLoaded", function() {
 													easing: 'lenear',
 													duration: 10000,
 													begin: function startExpire(){//Expire		
-														countdown = new Date(),
-														responseTime = new Date(Date.now() + (1000*10)); 
+														let countdown = new Date();
+														let responseTime = new Date(Date.now() + (1000*10)); 
 
 														(function startTime() {
 															let expire = document.querySelector('.expire');	
@@ -185,9 +196,12 @@ document.addEventListener("DOMContentLoaded", function() {
 															modal_3.style.display = 'none';
 															
 														anime({
-															delay: 2000,
+															targets:'.overlay',
+															opacity: 0,
+															duration: 100,
+															delay: 500,
 															begin: function(){
-																alert('Потрачено');
+																alert('Переход по ссылке!');
 															}
 														})	
 													}
@@ -214,6 +228,19 @@ document.addEventListener("DOMContentLoaded", function() {
 	//Appendix
 	(function Appendix () {
 		
+
+		const appendix = document.querySelector('#appendix'),
+			  logo = document.querySelector('#logo');
+
+		const likeCount = document.querySelector('#like_counter'),
+			  commentsCount = document.querySelector('#comments_counter');
+
+		const like_1 = document.querySelector('.like_image_1'),
+					like_2 = document.querySelector('.like_image_2'),
+					like_3 = document.querySelector('.like_image_3'),
+					like_4 = document.querySelector('.like_image_4');
+		
+
 		const a1 = document.querySelector("#a1"),
 		      p1 = document.querySelector("#p1"),
 		      a2 = document.querySelector("#a2"),
@@ -221,75 +248,111 @@ document.addEventListener("DOMContentLoaded", function() {
 		      a3 = document.querySelector("#a3"),
 		      p3 = document.querySelector("#p3"),
 		      a4 = document.querySelector("#a4"),
-		      p4 = document.querySelector("#p4"),
-		      a5 = document.querySelector("#a5"),
-		      p5 = document.querySelector("#p5");
+		      p4 = document.querySelector("#p4");
 
 		const num_1 = document.querySelector("#num_1"),
 		      num_2 = document.querySelector("#num_2"),
 		      num_3 = document.querySelector("#num_3"),
-		      num_4 = document.querySelector("#num_4"),
-		      num_5 = document.querySelector("#num_5");
+		      num_4 = document.querySelector("#num_4");
 
+		//Visible 
+		function off (a){
+			a.style.display = 'none';
+		}
+
+		//Like&love
+		// (function random_like (){
+		// 	let likeLoveImages = [
+		// 		'../img/_src/Love.png',
+		// 		'../img/_src/Like.png'
+		// 	]
+		// 	like_1.src = likeLoveImages[Math.floor(Math.random()*likeLoveImages.length)];
+		// 	like_2.src = likeLoveImages[Math.floor(Math.random()*likeLoveImages.length)];
+		// 	like_3.src = likeLoveImages[Math.floor(Math.random()*likeLoveImages.length)];
+		// 	like_4.src = likeLoveImages[Math.floor(Math.random()*likeLoveImages.length)];
+			
+		// })();
+		
+
+		//Like&Comments_counter
+		function like_counter (min, max){
+			let random = min + Math.random() * (max - min);
+			return Math.floor(random);
+		}
+		likeCount.innerHTML = like_counter(10345,18068);
+		let comments_counter = Number(likeCount.textContent);
+		commentsCount.innerHTML = comments_counter*5;
+		
+
+		//Comments			
 		const content_1a = function (a){
-			a1.append(a);
+			a1.innerHTML = a;
 		},
 		      content_1p = function (a){
-			p1.append(a);
+			p1.innerHTML = a;
 		},
 		      content_2a = function (a){
-			a2.append(a);
+			a2.innerHTML = a;
 		},
 		      content_2p = function (a){
-			p2.append(a);
+			p2.innerHTML = a;
 		},
 		      content_3a = function (a){
-			a3.append(a);
+			a3.innerHTML = a;
 		},
 		      content_3p = function (a){
-			p3.append(a);
+			p3.innerHTML = a;
 		},
 		      content_4a = function (a){
-			a4.append(a);
+			a4.innerHTML = a;
 		},
 		      content_4p = function (a){
-			p4.append(a);
+			p4.innerHTML = a;
 		};
 
-		const Number_1 = function (a){
+		const Time_1 = function (a){
 			num_1.append(a);
 		},
-		      Number_2 = function (a){
+		      Time_2 = function (a){
 			num_2.append(a);
 		},
-		      Number_3 = function (a){
+		      Time_3 = function (a){
 			num_3.append(a);
 		},
-		      Number_4 = function (a){
+		      Time_4 = function (a){
 			num_4.append(a);
 		};   
 
+
+		//....\/....Uncomment for off element
+		//off(appendix);
+		//off(logo);
+
+		
+		
+		
 		//First block 
 		content_1a("Victoria Johnson");  // name
 		content_1p("I had never won anything, but today I was lucky! Thank you!"); //text
-		Number_1("3"); //Minutes
+		Time_1("just now"); //Comment_time
 
 		//Second block
 		content_2a("Phil Graham");
-		content_2p("Yeap, i'm vinner. Timer it's my. Women, come to my car.");
-		Number_2("13");
+		content_2p("At first I thought this was just a joke, but the gift card arrived by the mail this morning");
+		
+		Time_2("4m");
 
 		//Three block
 		content_3a("Mike Hines");
-		content_3p("At first I thought this was just a joke, but the gift card arrived by the mail this morning! ");
-		Number_3("90");
+		content_3p("Sweeeet! I've won an iPhone!! My gosh!");
+		Time_3("10m");
 
 		//Four block
 		content_4a("Sandra Moore");
-		content_4p("Sweeeet! I've won an iPhone!! My gosh!");
-		Number_4("5");
+		content_4p("Initially I was not sure if this is real or not, but I have received my gift card today, thank you!!");
+		Time_4("1d");
 
 		})();
-
+		
 });
 
